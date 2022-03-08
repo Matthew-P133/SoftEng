196d09db15b3c690921b65a8c22b362a5406267e
Matthew-P133 <matthew.peters.work@gmail.com>
2022-03-08 16:10:53 +0000

    added Director class

---
+++ teachingManagementSystem/teachingManagementSystem/Director.java
@@ -0,0 +1,27 @@
+package teachingManagementSystem;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The Teacher class inherits the Staff class. 
+ * This class deals with the training data of teachers.
+ * @author Lin Yun Jen
+ *
+ */
+
+public class Director extends Staff implements Serializable{
+    // A set to store a teacher's training 
+ 
+    // Constructor
+    public Director(String name){
+       super(name);
+
+    }
+
+
+
+ 
+
+}
